f3c886a3c8a082a1953da1b7e8dc47a78938770e
John <john@ubuntu.(none)>
2012-10-13 01:49:24 -0400

    Implementing lane switching.

--- highway.py
+++ highway.py
@@ -6,6 +6,7 @@ class Highway:
 		"""Density is the number of cars per unit length."""
 		self.length = length
 		self.num_lanes = lanes
+        self.time = 0
         self.clear_road()
 		self.reset_density(density)
 
@@ -17,22 +18,92 @@ class Highway:
         elif populate_type == "random":
             filled_in_spots = [i for i in xrange(self.num_lanes*self.length) if (random.random()*self.density >= 0.5)]
 
-        # now populate those spots with vehicles
+        # set the spots to the desired vehicle type
+        self.set_vehicles(filled_in_spots, vehicle_type)
+
+    def set_vehicles(self, filled_in_spots, vehicle_type="car"):
+        obj_type = Car
         if vehicle_type == "car":
-            for spot in filled_in_spots:
-                self.road[spot // self.num_lanes][spot % self.num_lanes] = Car()
-        else:
-            # default action is to populate the road with cars 
-            for spot in filled_in_spots:
-                self.road[spot // self.num_lanes][spot % self.num_lanes] = Car()
+            obj_type = Car
+        elif vehicle_type == "pickup_truck":
+            obj_type = PickupTruck 
+        elif vehicle_type == "semi_truck":
+            obj_type = SemiTruck 
+        elif vehicle_type == "suv":
+            obj_type = SUV 
+
+        for spot in filled_in_spots:
+            new_vehicle = obj_type()
+            i = spot // self.num_lanes
+            j = spot % self.num_lanes
+            self.road[i][j] = new_vehicle
+            self.vehicles[(i,j)] = new_vehicle
 
     def clear_road(self):
 		self.road = [[UnoccupiedLane() for i in xrange(self.num_lanes)] for j in xrange(self.length)]
+        self.vehicles = {}
 
 	def reset_density(self, density):
 		self.density = density
         self.populate_road("fixed_width")
-			
+
+    def get_next_vehicle_position(self, start_place, lane, forward=True):
+        """Gets the position of the next vehicle in the specified lane, returns none if there are no vehicles in front."""
+        if forward:
+            start = start_place+1
+            end = self.length
+            increment = 1
+        else:
+            start = start_place-1
+            end = -1
+            increment = -1
+        for i in xrange(start, end, increment):
+            if self.vehicles[(i,lane)]:
+                return (i, lane) 
+        return None
+
+
+    def next_time_step(self):
+        self.time += 1
+        for x in xrange(len(self.road)):
+            for spot_num in xrange(len(position)):
+                # check how far up the car can move 
+                current_vehicle = self.road[x][spot_num] 
+                next_vehicle_position = self.get_next_vehicle_position(x, spot_num)
+                if next_vehicle_position:
+                    # if there is a vehicle in front, we move up to the follow distance of the vehicle, or change lanes probabilistically
+                    should_switch = current_vehicle.should_switch_lanes(x, next_vehicle_position[0])
+
+    def switch_lanes(self, vehicle_position):
+        (x, y) = vehicle_position
+        # try switching to the left lane
+        if y+1 < self.num_lanes:
+            # check if there is someone behind
+            previous_vehicle_position = self.get_next_vehicle_position(x, y+1, False)
+            previous_vehicle = self.vehicles[previous_vehicle_position]
+            # 
+        elif y-1 >= 0:
+            previous_vehicle_position = self.get_next_vehicle_position(x, y-1, False)
+
+    def make_lane_switch(self, switching_vehicle_position, incoming_vehicle_position, new_lane):
+        switching_vehicle = self.vehicles[switching_vehicle_position]
+        if incoming_vehicle_position:
+            incoming_vehicle = self.vehicles[incoming_vehicle_position]
+            distance = switching_vehicle_position[0] - incoming_vehicle_position[0]
+
+            if incoming_vehicle.speed <= switching_vehicle.speed:
+                # if the incoming vehicle is slower than the switching vehicle, we only care to do anything when the distance is too close
+                if distance < incoming_vehicle.follow_distance:
+                    # incoming vehicle needs to slow down to allow appropriate follow distance
+                    incoming_vehicle.speed = incoming_vehicle.follow_distance - distance
+            elif distance < incoming_vehicle_speed - switching_vehicle.speed:
+                # going to crash, definitely need to try to slow down and switch
+
+        # switch the lane
+        self.vehicles[(swiching_vehicle_position[0], new_lane)] = switching_vehicle
+        self.vehicles[switching_vehicle_position] = UnoccupiedLane()
+
+
 
 class LaneOccupier(object):
 	def __init__(self, length, width, is_accident=False):
@@ -57,29 +128,32 @@ class Accident(LaneOccupier):
 		self.time_left -= 1
 
 class Vehicle(LaneOccupier):
-	def __init__(self, size, speed=60):
+	def __init__(self, size, speed, follow_distance = 10):
 		super(Vehicle, self).__init__(size, 1, False)
 		self.size = size
         self.speed = speed
+        self.follow_distance = follow_distance
 
-	def get_speed(self):
-		return (1.0/math.log(self.size+1))
+    def should_switch_lanes(self, start_position, next_vehicle_position):
+        distance = next_vehicle_position - start_position
+        if distance - self.follow_distance < self.speed:
+            return True
 
 class Car(Vehicle):
 	def __init__(self):
-		super(Car, self).__init__(1)
+		super(Car, self).__init__(1, 70)
 
 class PickupTruck(Vehicle):
 	def __init__(self):
-		super(Car, self).__init__(2)
+		super(Car, self).__init__(2, 65)
 
 class SemiTruck(Vehicle):
 	def __init__(self):
-		super(Car, self).__init__(5)
+		super(Car, self).__init__(5, 55)
 
 class SUV(Vehicle):
 	def __init__(self):
-		super(Car, self).__init__(2)
+		super(Car, self).__init__(2, 65)
 
 if __name__ == '__main__':
 	a = Car()
